2a14bb69bab1e41651df829a0b5bdc49ccab19ba
Pål-Erik Martinsen <paal.martinsen@tandberg.com>
2011-03-29 10:06:43 +0200

    More unittests.

---
+++ test/icelibtypes_test.c
@@ -0,0 +1,64 @@
+
+#include <check.h>
+
+#include "icelibtypes.h"
+
+START_TEST( candidate_toString )
+{
+    fail_unless( 0 == strcmp(ICELIBTYPES_ICE_CANDIDATE_TYPE_toString(ICE_CAND_TYPE_NONE), "NONE") );
+    fail_unless( 0 == strcmp(ICELIBTYPES_ICE_CANDIDATE_TYPE_toString(ICE_CAND_TYPE_HOST), "HOST") );
+    fail_unless( 0 == strcmp(ICELIBTYPES_ICE_CANDIDATE_TYPE_toString(ICE_CAND_TYPE_SRFLX), "SRFLX") );    
+    fail_unless( 0 == strcmp(ICELIBTYPES_ICE_CANDIDATE_TYPE_toString(ICE_CAND_TYPE_RELAY), "RELAY") );
+    fail_unless( 0 == strcmp(ICELIBTYPES_ICE_CANDIDATE_TYPE_toString(ICE_CAND_TYPE_PRFLX), "PRFLX") );
+    fail_unless( 0 == strcmp(ICELIBTYPES_ICE_CANDIDATE_TYPE_toString(24), "UNKNOWN") );
+
+}
+END_TEST
+
+START_TEST( candidate_dump )
+{
+    ICE_CANDIDATE candidate;
+
+    ICELIBTYPES_ICE_CANDIDATE_dump(stderr, &candidate);
+
+}
+END_TEST
+
+START_TEST( mediastream_dump )
+{
+
+    ICE_MEDIA_STREAM iceMediaStream;
+
+    ICELIBTYPES_ICE_MEDIA_STREAM_reset(&iceMediaStream);
+
+    ICELIBTYPES_ICE_MEDIA_STREAM_dump(stderr, &iceMediaStream);
+}
+END_TEST
+
+
+START_TEST( media_dump )
+{
+    ICE_MEDIA iceMedia;
+    ICELIBTYPES_ICE_MEDIA_reset(&iceMedia);
+
+    ICELIBTYPES_ICE_MEDIA_dump(stderr, &iceMedia);
+
+}
+END_TEST
+
+
+Suite * icelibtypes_suite (void)
+{
+  Suite *s = suite_create ("ICElibtypes");
+
+  {/* Core test case */
+      TCase *tc_core = tcase_create ("Core");
+      tcase_add_test (tc_core, candidate_toString);
+      tcase_add_test (tc_core, candidate_dump);
+      tcase_add_test (tc_core, mediastream_dump);
+      tcase_add_test (tc_core, media_dump );
+      suite_add_tcase (s, tc_core);
+  }
+
+  return s;
+}
