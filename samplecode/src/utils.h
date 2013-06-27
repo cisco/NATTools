77538062be34e608ea3ee775e083fd8f45ae8828
Julius Asplin <jasplin@cisco.com>
2013-06-27 09:26:53 +0200

    add createSocket()

--- samplecode/src/utils.h
+++ samplecode/src/utils.h
@@ -1,1 +1,2 @@
 int recvStunMsg(int sockfd, struct sockaddr_storage *their_addr, StunMessage *stunResponse, unsigned char *buf);
+int createSocket(char server[], char serverport [], char outprintName[], bool bind, struct addrinfo *p);
