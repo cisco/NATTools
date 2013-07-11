fbb14dbbb8d3a80c61c3ea623a8bd28410f31e45
Martin Gammelsæter <gammarti@cisco.com>
2013-07-11 10:50:00 +0200

    Differentiate between no message/no buffer errormsg in decoding.

--- stunlib/src/stunlib.c
+++ stunlib/src/stunlib.c
@@ -1676,9 +1676,16 @@ stunlib_DecodeMessage(unsigned char* buf,
     int restlen = bufLen;
     StunAtrHdr sAtr;
 
-    if (!buf || !message)
+    if (!buf)
     {
-        if (stream != NULL) printError(stderr, "No buffer or no message recieved\n");
+        if (stream != NULL) printError(stderr, "No buffer recieved\n");
+        return false;
+    }
+
+
+    if (!message)
+    {
+        if (stream != NULL) printError(stderr, "No message recieved\n");
         return false;
     }
 
