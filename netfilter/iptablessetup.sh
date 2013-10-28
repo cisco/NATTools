915e8068a74087c1681de0fc0b1c0965f6630112
Martin Gammelsæter <gammarti@cisco.com>
2013-10-28 16:45:43 +0100

    Select all STUN packets on all ports.

--- netfilter/iptablessetup.sh
+++ netfilter/iptablessetup.sh
@@ -3,8 +3,7 @@
 # Flush all rules.
 iptables -F
 
-# Capture every udp-packet with either destport or srcport as 4950,
-# since that is the port we use for stun-testing.
-# They are then punted to NFQUEUE number 0.
-iptables -A FORWARD -p udp --dport 4950 -j NFQUEUE --queue-num 0
-iptables -A FORWARD -p udp --sport 4950 -j NFQUEUE --queue-num 0
+# Capture every STUN-packet as identified by the magic cookie, and punt them to
+# NFQUEUE number 0. The string module could be used instead for more clarity,
+# but u32 is much faster in this case.
+iptables -A FORWARD -p udp --match u32 --u32 "0>>22&0x3C@12=0x2112a442" -j NFQUEUE --queue-num 0
