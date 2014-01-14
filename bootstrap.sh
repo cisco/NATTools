fa1b711e809e400d85beea2c66d26959d7b16dd5
Pål-Erik Martinsen <palmarti@cisco.com>
2014-01-14 13:25:07 +0100

    Fixed script so it actually works.. (Relative paths.)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-icelib/bootstrap.sh
-stunlib/bootstrap.sh
-sockaddrutil/bootstrap.sh
+mkdir m4 build-aux
+autoreconf --install
+cd icelib
+./bootstrap.sh
+cd ../stunlib
+./bootstrap.sh
+cd ../sockaddrutil
+./bootstrap.sh
 
 
